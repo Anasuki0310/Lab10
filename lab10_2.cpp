48eedc71ed657336aa3d7ee0fbb333c5c63fcfd5
Anasuki0310 <anatsapong_luechai@cmu.ac.th>
2024-12-17 23:53:19 +0700

    complete

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -9,8 +9,35 @@ int main (){
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int main (){
+	ifstream source;
+	ofstream dest;
+	source.open("cheerbook.txt");
+	dest.open("cheerbook_copy.txt");
+	
+	string textline;
+	
+	dest << "-------------------- BOOM ---------------------" << endl;
+	
+	while(getline(source, textline)){
+	    dest << textline << endl;
+	    
+	    
+	}
+	
+	dest << "-------------------- HA!! ---------------------";
+	
+	
+
+    source.close();
+    dest.close();
+	return 0;
+}
 
     source.close();
     dest.close();
